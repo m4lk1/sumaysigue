bfaa49822b7689b83b68e1e4a45c7561df1f3e05
m4lk1 <laughoutloud90@gmail.com>
2016-04-22 12:37:35 +0200

    Controlador Finalizado

---
+++ controlador/controlador.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require "../vendor/autoload.php";
+
+ $saludoRecibido= $_GET['saludo'];
+ 
+ use sumaysigue\modelo\saludo;
+ 
+ $objetoSaludo=new saludo();
+ $objetoSaludo->setSaludo($saludoRecibido);
+ 
+ $saludo = $objetoSaludo->formatSaludo();
+ 
+ require "../vista/plantillasaludo.php";
